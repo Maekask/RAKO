51501541758792fb9f88f18ebacde727252782ec
Mart Mäekask <maekask@gmail.com>
2025-09-18 16:02:52 +0300

    18.09

--- Phython/loops.py
+++ Phython/loops.py
@@ -23,3 +23,31 @@ print('\n') # Kaks reavahetust!
 
 for x in range(0, 10, 2): # 0 kuni 10, sammuga 2
     print(x, end=' | ')
+
+print('\n')
+
+x = 0
+while x < len(names):
+    print(names[x])
+    x += 1 # x = x + 1  
+print(x)
+
+# Väljasta listi nimed konsooli iga nimi eraldi real, kuid iga nime ees on järjekorra number. järjekorra number algab ühega. Korrektne rida on järgnev:
+# 1. Mari
+# 2. Jüri 
+# 3. Paul
+# 4. Kati
+for x in range(len(names)):
+    print(f"{x + 1}. {names[x]}")
+
+print()
+
+# TÄIENDUS: tee igale nimele juhuslik vanus, kuid kirjuta see vanus listi nimega ages. Näita tulemust sama for või while loopis. Peale kordust näita nii names, kui ages listi sisu lihsalt nagu listid.py failis näitasime.
+
+ages = []
+for x in range(len(names)):
+    ages.append(random.randint(1, 122))
+    print(f"{x + 1}. {names[x]} - {ages[x]}")       
+
+print(names)
+print(ages)
