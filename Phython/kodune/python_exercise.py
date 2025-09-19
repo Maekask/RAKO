5df0d153ad76182de32484646ac8e3b14f6f26e6
Maekask <156500170+Maekask@users.noreply.github.com>
2025-09-19 20:17:19 +0300

    homework

---
+++ Phython/kodune/python_exercise.py
@@ -0,0 +1,30 @@
+# 1. Genereerib 20 juhuslikku täisarvu vahemikus 1–100.
+import random
+arvud = [random.randint(1, 100) for _ in range(20)] # Genereerib 20 juhuslikku täisarvu vahemikus 1–100
+
+# 2. Salvestab need koos tänase kuupäevaga faili andmed.txt.
+from datetime import datetime
+date = datetime.now().strftime("%d.%m.%Y %H:%M:%S") #  Tänane kuupäev ja aeg
+with open("Phython/kodune/andmed.txt", "w") as f: # Ava fail kirjutamiseks
+    f.write(f"Kuupäev: {date}\n") # Kirjuta kuupäev faili
+    f.write("Arvud: " + " ".join(map(str, arvud)) + "\n") # Kirjuta arvud faili  
+
+# 3. Loeb failist need arvud uuesti sisse.
+with open("Phython/kodune/andmed.txt", "r") as f: # Ava fail lugemiseks
+    contents = f.readlines() # Loe kõik read
+    loe_arvud = list(map(int, contents[1].strip().split(": ")[1].split())) # Võtab teisest failireast arvud
+print("Failist loetud arvud:", loe_arvud)
+
+# 4. Kasutab funktsiooni (enda loodud funktsioon), mis arvutab: summa, keskmise ja suurima arvu.
+def arvuta_andmed(arvud):
+    summa = sum(arvud) # Arvuta summa
+    keskmine = summa / len(arvud) # Arvuta keskmine
+    suurim = max(arvud) # Leia suurim arv
+    return summa, keskmine, suurim  # Tagasta tulemused
+summa, keskmine, suurim = arvuta_andmed(loe_arvud) # Kutsu funktsioon
+print(f"Summa: {summa}\nKeskmine: {keskmine}\nSuurim arv: {suurim}") # Prindi tulemused
+
+
+
+
+
