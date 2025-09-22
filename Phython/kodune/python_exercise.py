fd9a8c5358ee920618264b8c0c1603342597c2f9
Maekask <156500170+Maekask@users.noreply.github.com>
2025-09-22 18:36:11 +0300

    kodune

--- Phython/kodune/python_exercise.py
+++ Phython/kodune/python_exercise.py
@@ -1,16 +1,19 @@
 # 1. Genereerib 20 juhuslikku täisarvu vahemikus 1–100.
 import random
+from datetime import datetime
+
+file = "Phython/kodune/andmed.txt"
+# 1. Genereerib 20 juhuslikku täisarvu vahemikus 1–100.
 arvud = [random.randint(1, 100) for _ in range(20)] # Genereerib 20 juhuslikku täisarvu vahemikus 1–100
 
 # 2. Salvestab need koos tänase kuupäevaga faili andmed.txt.
-from datetime import datetime
 date = datetime.now().strftime("%d.%m.%Y %H:%M:%S") #  Tänane kuupäev ja aeg
-with open("Phython/kodune/andmed.txt", "w") as f: # Ava fail kirjutamiseks
+with open(file, "w") as f: # Ava fail kirjutamiseks
     f.write(f"Kuupäev: {date}\n") # Kirjuta kuupäev faili
     f.write("Arvud: " + " ".join(map(str, arvud)) + "\n") # Kirjuta arvud faili  
 
 # 3. Loeb failist need arvud uuesti sisse.
-with open("Phython/kodune/andmed.txt", "r") as f: # Ava fail lugemiseks
+with open(file, "r") as f: # Ava fail lugemiseks
     contents = f.readlines() # Loe kõik read
     loe_arvud = list(map(int, contents[1].strip().split(": ")[1].split())) # Võtab teisest failireast arvud
 print("Failist loetud arvud:", loe_arvud)
